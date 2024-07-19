735a6ffbf132712206c8b8d56054be43b44777f4
Mehdi Mollaverdi <mmollaverdi@squareup.com>
2024-07-19 14:53:17 +1000

    Adds the core plugin functionality

---
+++ plugin/src/main/kotlin/xyz/block/tools/monorepo/idea/InactiveProjectExcludeDirPolicy.kt
@@ -0,0 +1,43 @@
+package xyz.block.tools.monorepo.idea
+
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.impl.DirectoryIndexExcludePolicy
+import java.nio.file.Paths
+
+class InactiveProjectExcludeDirPolicy(private val project: Project?) : DirectoryIndexExcludePolicy {
+
+  override fun getExcludeUrlsForProject(): Array<out String> {
+    if (project == null) {
+      logger.info("The project is null - skipping")
+      return emptyArray()
+    }
+
+    val inactiveProjectDirs = project.getInactiveProjectDirs()
+    return if (inactiveProjectDirs.isNotEmpty()) {
+      logger.info("Excluding inactive project dirs: $inactiveProjectDirs")
+      inactiveProjectDirs.map { Paths.get(project.basePath, it) }.map { "file://$it" }.toSet().toTypedArray()
+    } else {
+      emptyArray()
+    }
+  }
+
+  companion object {
+    private val logger = Logger.getInstance(InactiveProjectExcludeDirPolicy::class.java)
+
+    private fun Project.getInactiveProjectDirs(): List<String> {
+      val requestedModulesContent = this.getFileContent("requested-modules.txt") ?: return emptyList()
+      val skipDirExclusions = requestedModulesContent.split("\n").any { it.trim() == "__SKIP_DIR_EXCLUSION" }
+      if (skipDirExclusions) {
+        logger.info("Skipping exclude of inactive project dirs as requested by user")
+        return emptyList()
+      }
+
+      val inactiveProjectsContent = this.getFileContent("settings_inactive_modules.txt") ?: return emptyList()
+      return inactiveProjectsContent
+        .split("\n")
+        .filter { it.trim().isNotEmpty() }
+        .map { it.replace(':', '/').trim('/') }
+    }
+  }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+import org.jetbrains.intellij.platform.gradle.extensions.intellijPlatform
+
+rootProject.name = "gradle-monorepo"
+
+pluginManagement {
+  repositories {
+    mavenCentral()
+    gradlePluginPortal()
+  }
+}
+
+plugins {
+  id("org.jetbrains.intellij.platform.settings") version "2.1.0"
+}
+
+dependencyResolutionManagement {
+  repositories {
+    mavenCentral()
+    intellijPlatform {
+      defaultRepositories()
+    }
+  }
+}
+
+include(":plugin")
