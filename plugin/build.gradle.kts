932df1d1206df3b83bd2c7d0fce4f1dfb0e36cae
Mehdi Mollaverdi <m.mollaverdi@gmail.com>
2025-08-06 00:41:30 +1000

    Force refresh of the project view (#23)
    * Attempt at forcing a project view refresh
    
    * Try FileStatusManager.getInstance(project).fileStatusesChanged()
    
    * Upgrade 'since version' used for verification to address memory errors on CI

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -10,6 +10,7 @@ dependencies {
   intellijPlatform {
     intellijIdeaUltimate("2024.3.2")
     pluginVerifier("1.381")
+    bundledPlugin("com.intellij.gradle")
   }
 }
 
@@ -17,7 +18,7 @@ version = System.getenv("IJ_PLUGIN_VERSION") ?: "0.1.0" // IJ_PLUGIN_VERSION env
 
 val pluginName = "gradle-monorepo"
 val sinceBuildMajorVersion = "233" // corresponds to 2023.3.x versions
-val sinceIdeVersionForVerification = "241.14494.240" // corresponds to the 2024.1 version
+val sinceIdeVersionForVerification = "243.21565.193" // corresponds to the 2024.3 version
 val untilIdeVersion = properties["IIC.release.version"] as String
 val untilBuildMajorVersion = untilIdeVersion.substringBefore('.')
 
