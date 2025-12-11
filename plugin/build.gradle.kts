447a5ecd9c64692a7a814bc2de7807bcc60f69c2
Mehdi Mollaverdi <mmollaverdi@squareup.com>
2025-12-11 23:45:25 +1100

    Support IntelliJ version 2025.3

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
   implementation(libs.kotlinStdLib)
   intellijPlatform {
     intellijIdeaUltimate("2024.3.2")
-    pluginVerifier("1.381")
+    pluginVerifier("1.388")
     bundledPlugin("com.intellij.gradle")
   }
 }
@@ -17,8 +17,8 @@ dependencies {
 version = System.getenv("IJ_PLUGIN_VERSION") ?: "0.1.0" // IJ_PLUGIN_VERSION env var available in CI
 
 val pluginName = "gradle-monorepo"
-val sinceBuildMajorVersion = "233" // corresponds to 2023.3.x versions
-val sinceIdeVersionForVerification = "243.21565.193" // corresponds to the 2024.3 version
+val sinceBuildMajorVersion = "241" // corresponds to 2024.1.x versions
+val sinceIdeVersionForVerification = "243.28141.18" // corresponds to the 2024.3 version
 val untilIdeVersion = properties["IIC.release.version"] as String
 val untilBuildMajorVersion = untilIdeVersion.substringBefore('.')
 
